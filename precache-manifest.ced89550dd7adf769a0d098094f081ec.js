3ba2eb0cb4c321d965cc820157bb6be6fe89375d
HpFreake-01 <maxchuprun01@gmail.com>
2020-09-30 08:07:02 +0300

    Updates

---
+++ precache-manifest.ced89550dd7adf769a0d098094f081ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88b9d94421687f58de6bb25ab2ba8b79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f4aa86ddd5fd9672962",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "41b02a035f87ff2040e2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3f4aa86ddd5fd9672962",
+    "url": "/static/js/2.3dc16935.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.3dc16935.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41b02a035f87ff2040e2",
+    "url": "/static/js/main.097f61db.chunk.js"
+  },
+  {
+    "revision": "1733d1d6c8050457218c",
+    "url": "/static/js/runtime-main.d66d9b46.js"
+  },
+  {
+    "revision": "c93a5e3409e6c914c4cede50cd286f1e",
+    "url": "/static/media/sunsetimg.c93a5e34.jpg"
+  }
+]);
